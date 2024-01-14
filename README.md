b9794500f6eb9280852be6b10eed6661f1479548
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-01-14 10:29:55 +0100

    reamde

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# thorium-server
+# Thorium Server
+
+Thorium Server est un serveur express personnalisable qui peut être utilisé comme module ou incorporé dans le thorium-cli (un utilitaire en ligne de commande pour compiler, servir ou créer un projet Thorium).
+
+## Installation
+
+Pour installer Thorium Server, vous pouvez utiliser npm :
+
+```bash
+npm install thorium-server
+```
+
+## Utilisation
+
+```typescript
+import { preloadConfiguration, ThoriumServer } from 'thorium-server';
+
+// Charger la configuration du serveur
+const server: ThoriumServer = await preloadConfiguration();
+
+// Le serveur est prêt, vous pouvez l'utiliser selon vos besoins.
+```
+
+## Configuration
+
+Thorium Server peut être configuré en utilisant un fichier server.config.json situé à la racine du projet. 
+
+Voici un exemple de fichier de configuration :
+
+```json
+{
+  "main": "src/main.js",
+  "package": "build/myApp.zip",
+  "devServer": {
+    "port": 3000
+  },
+  "routes": ["src/routes/api.js", "src/routes/web.js"],
+  "debug": true,
+  "client": {
+    "views": {
+      "/home": "src/views/home.hbs",
+      "/about": "src/views/about.hbs"
+    }
+  }
+}
+```
+
+## Propriétés de configuration :
+
+- main (facultatif): Chemin vers le fichier principal de l'application à exécuter.
+- package (facultatif): Chemin vers un fichier zip ou une solution contenant le code source emballé.
+- devServer (facultatif): Options de configuration pour le webpack-dev-server.
+- routes (facultatif): Chemins vers les scripts de routeurs à charger.
+- debug (facultatif): Activer/désactiver le mode débogage.
+- client (facultatif): Options spécifiques au client.
+- client.views (facultatif): Chemins vers les fichiers de vue et les routes correspondantes.
+
+# Exemples
+
+Configuration minimale :
--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,8 @@ serverMessage.color = colors.blue;
 `serverSucces` that is an extension of the `serverLog` debug function. This allows you to log
 messages with a specific tag, in this case, "succes". The `serverSucces` function will output
 messages in green color when called. */
-let serverSucces = serverLog.extend('succes');
-serverSucces.color = colors.green;
+let serverSuccess = serverLog.extend('success');
+serverSuccess.color = colors.green;
 
 /** The line is creating a new debug function called
 `serverWarning` that is an extension of the `serverLog` debug function. This allows you to log
@@ -55,8 +55,8 @@ serverError.color = colors.red;
  * @param {string} message - The parameter "message" is a string that represents the message to be
  * printed.
  */
-function printSucces( message:string ){
-  serverSucces( String(message).green );
+function printSuccess( message:string ){
+  serverSuccess( String(message).green );
 }
 
 /**
@@ -105,12 +105,52 @@ let devServer:Express | Server['app'] = express();
 
 /** The `IThoriumServerConfiguration` interface defines the structure of the server configuration object used in the Thorium Server. It has the following properties: */
 interface IThoriumServerConfiguration{
+
+  /** The `main?:string;` line is defining a property called `main` in the `IThoriumServerConfiguration`
+  interface. The `main` property is of type `string` and represents the path to the main.js file of
+  the application. This file can have any name. If the `main` property is defined, it will be
+  executed by passing the Express server as an argument and retrieving it as the output. This allows
+  you to modify the server. */
   main?:string;
+
+  /** The `package?:string;` line is defining a property called `package` in the
+  `IThoriumServerConfiguration` interface. The `package` property is of type `string` and represents
+  the path to a zip file or a `solution` containing the result of packaging the source code using
+  `thorium-cli`. This package will be used by the server to establish static routes and distribute
+  the content of the solution. */
   package?:string;
+
+  /** The `devServer?:DevServerConfiguration;` line is defining an optional property called `devServer`
+  in the `IThoriumServerConfiguration` interface. The `devServer` property is of type
+  `DevServerConfiguration`, which represents the configuration options for the webpack-dev-server.
+  This property allows you to specify custom configuration options for the webpack-dev-server when
+  using it in the Thorium Server. If the `devServer` property is not provided in the server
+  configuration, it will default to `null`. */
   devServer?:DevServerConfiguration;
+
+  /** The `routes?:string[];` line is defining an optional property called `routes` in the
+  `IThoriumServerConfiguration` interface. The `routes` property is an array of strings that
+  represents the paths to the router scripts that will be loaded and used by the server. Each string
+  in the array should be a valid file path to a router script. If the `routes` property is provided
+  in the server configuration, the Thorium Server will load and use the specified router scripts to
+  handle different routes and endpoints. */
   routes?:string[];
+
+  /** The `debug?:boolean;` property in the `IThoriumServerConfiguration` interface is an optional
+  property that allows you to enable or disable debug mode for the Thorium Server. If the `debug`
+  property is set to `true`, it means that debug mode is enabled and additional debug information
+  will be logged. If the `debug` property is not provided or set to `false`, debug mode is disabled
+  and no additional debug information will be logged. */
   debug?:boolean;
+
+  /** The `client?` is a property in the `IThoriumServerConfiguration` interface that represents the
+  client-specific configuration options for the Thorium Server. It allows you to specify additional
+  configuration options related to the client-side of the application. */
   client?:{
+
+    /** The `views?: Record<string,string>` property in the `client` property of the
+    `IThoriumServerConfiguration` interface represents the views that will be loaded and used by the
+    Thorium Server. */
     views?: Record<string,string>
   }
 }
@@ -126,7 +166,7 @@ export type ThoriumServer = Express | Server;
  * @param {Express | Server['app']} [server] - The `server` parameter is an optional parameter that
  * represents the Express server or the `app` property of a `Server` object. It is used to determine
  * whether the server configuration is for a webpack dev server or a production server. If the `server`
- * parameter is provided, it means that the server is in production mode
+ * parameter is provided, it means that the server is in developpment mode
  * @returns the `devServer` object.
 */
 export const preloadConfiguration = async ( serverDirPath = process.env.PWD || process.cwd() , server?: Express | Server['app'] ) => {
@@ -155,7 +195,7 @@ export const preloadConfiguration = async ( serverDirPath = process.env.PWD || p
   };
 
   if(fs.existsSync(serverFileConfiguration)){
-    printSucces( `server.config.json found in ${serverDirPath}` );
+    printSuccess( `server.config.json found in ${serverDirPath}` );
     serverConfiguration = JSON.parse( fs.readFileSync( serverFileConfiguration , 'utf-8' ) );
 
     if(server){
@@ -221,9 +261,11 @@ export const preloadConfiguration = async ( serverDirPath = process.env.PWD || p
       if(!server)for(const route of Object.keys(views)){
         let filePath = path.join(serverDirPath , views[route]);
         (devServer as Express).get( route , ( req , res , next ) => {
+
           let template = handlbars.compile( fs.readFileSync( filePath , 'utf-8' ) );
           let result = template( { build : 'build.js' } );
           res.send( result )
+          
         })
       }
       
